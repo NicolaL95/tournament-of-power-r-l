682b0c67f49efef9bdd4001356d6b7b3ba32d3db
Emanuele Marigliano <emamari96@live.it>
2023-10-09 21:36:16 +0200

    Add comment about a Beast

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          torneo del potere
+         Tegus BESTIA
         </p>
         <a
           className="App-link"
