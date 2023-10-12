5e09d33588ab309c8d2cb4356b24221a0ff0dc71
NicolaL95 <nicola.lilla@gmail.com>
2023-10-13 00:30:56 +0200

    add bulma

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 const Header = () =>{
 return(
     <header className='container' style={{backgroundColor: 'red'}}>
-        <div className="row">
-            <div style={{backgroundColor: 'blue', height: '50px'}}></div>
-            <div style={{backgroundColor: 'green', height: '50px'}}></div>
+        <div className="columns m-0">
+            <div className="column is-3" style={{backgroundColor: 'blue', height: '20px'}}></div>
+            <div className="column is-9" style={{backgroundColor: 'green', height: '20px'}}></div>
         </div>
     </header>
 
