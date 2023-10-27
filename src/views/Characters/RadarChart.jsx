4fe3ff60883eb45d5a551a2d7f412581e35661e5
KJWXY <emamari96@live.it>
2023-10-27 11:51:33 +0200

    test

--- src/views/Characters/RadarChart.jsx
+++ src/views/Characters/RadarChart.jsx
@@ -6,7 +6,7 @@ export default function RadarChart({characterSpecs2k21, characterSpecs2k24}) {
 
   const options = DB.radarChart
   options.series[0].data[0].value = characterSpecs2k21
-  options.series[0].data[0].value = characterSpecs2k24
+  options.series[0].data[1].value = characterSpecs2k24
 
   useEffect(() => {
     const chartDomLoocation = document.getElementById('radarChart');
